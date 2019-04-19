23b0cb2af3ae1274b8fdac7ed94520154d97cf88
凌亦 <lingyi.zcs@alibaba-inc.com>
2019-04-19 12:51:21 +0800

    release(1.6.8): release 1.6.8

--- site_scripts/_site/src/app/app.component.ts
+++ site_scripts/_site/src/app/app.component.ts
@@ -28,7 +28,7 @@ export class AppComponent implements OnInit, AfterViewInit {
     '0.6.x',
     '0.7.x'
   ];
-  currentVersion = '1.6.7';
+  currentVersion = '1.6.8';
 
   @ViewChild('searchInput') searchInput: ElementRef<HTMLInputElement>;
 
